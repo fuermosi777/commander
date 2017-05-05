15c4c32aa1dad049089824052862c4c51f48cb94
Hao Liu <haol@yahoo-inc.com>
2017-05-05 15:28:51 -0700

    update

--- main.js
+++ main.js
@@ -64,5 +64,9 @@ const menubar = require('menubar');
 let mb = menubar();
 
 mb.on('ready', () => {
-  console.log('app is ready');
+  
+});
+
+mb.on('after-create-window', () => {
+  mb.window.openDevTools();
 });
